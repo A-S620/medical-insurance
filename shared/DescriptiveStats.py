dfd0e3693908d9bd247d9b77907cc3a6d500c1ef
A-S620 <aatikaseedat00@gmail.com>
2022-08-05 21:42:42 +0100

    created DescriptiveStats class did descriptive statistics for numerical values in data set TODO: show results in a table

---
+++ shared/DescriptiveStats.py
@@ -0,0 +1,9 @@
+class DescriptiveStats:
+    def __init__(self, mean, median, mode, range_stat, interquartile_range, standard_deviation, variance):
+        self.mean = mean
+        self.median = median
+        self.mode = mode
+        self.range_stat = range_stat
+        self.interquartile_range = interquartile_range
+        self.standard_deviation = standard_deviation
+        self.variance = variance
